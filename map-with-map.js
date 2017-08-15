648f311b6a6065f6c006f66fcf3aead6e67b6f5e
Daniel Marell <daniel.marell@gmail.com>
2017-08-15 16:12:06 +0200

    first commit

---
+++ map-with-map.js
@@ -0,0 +1,55 @@
+/*
+  Map implemented with a javascript Map class.
+*/
+class MapWithMap {
+  static runTest () {
+    const map = new Map()
+
+    map.set('key1', 'value1')
+    map.set('key2', 'value2')
+    map.set('key3', 'value3')
+
+    console.log('  --- for keys')
+    for (let k of map.keys()) {
+      console.log('    ', k)
+    }
+
+    console.log('  --- for values')
+    for (let v of map.values()) {
+      console.log('    ', v)
+    }
+
+    console.log('  --- for entries()')
+    for (let e of map.entries()) {
+      console.log('    ', e)
+    }
+
+    console.log('  --- for entries [k,v]')
+    for (let [k, v] of map) {
+      console.log('    ', k, '   ', v)
+    }
+
+    // Drawback: filter,map,reduce not implemented for Map
+    console.log('  --- map forEach value filter')
+    {
+      const filtered = []
+      map.forEach((v, k) => { if (k === 'key2') filtered.push(v) })
+      filtered.map(e => console.log('    ', e))
+    }
+
+    // Drawback: Copy to array then use filter() and map()
+    console.log('  --- filter keys')
+    Array.from(map.keys()).filter(k => k === 'key2').map(e => console.log('    ', e))
+
+    // Drawback: Copy to array then use filter() and map()
+    console.log('  --- filter values')
+    Array.from(map.values()).filter(v => v === 'value2').map(e => console.log('    ', e))
+
+    // Drawback: Copy to array then use filter() and map()
+    console.log('  --- filter entries')
+    Array.from(map.entries()).filter(e => e.key === 'key2').map(e => console.log('    ', e)) // TODO not working
+  }
+}
+
+console.log('MapWithMap-------------------------------------------')
+MapWithMap.runTest()
---
+++ map-with-object.js
@@ -0,0 +1,46 @@
+/*
+  Map implemented with a plain javascript object.
+*/
+class MapWithObject {
+  static runTest () {
+    const map = {}
+
+    map['key1'] = 'value1'
+    map['key2'] = 'value2'
+    map['key3'] = 'value3'
+
+    console.log('  --- for keys')
+    for (let k in map) {
+      console.log('    ', k)
+    }
+
+    console.log('  --- for values (and keys)')
+    for (let k in map) {
+      const v = map[k]
+      console.log('    ', v)
+    }
+
+    console.log('  --- for entries')
+    for (let k in map) {
+      const v = map[k]
+      const e = {key: k, value: map[k]}
+      console.log('    ', e)
+    }
+
+    console.log('  --- filter keys')
+    Object.keys(map).filter(k => k === 'key2').map(e => console.log('    ', e))
+
+    console.log('  --- filter values')
+    Object.values(map).filter(k => k === 'value2').map(e => console.log('    ', e))
+
+    console.log('  --- filter entries')
+    MapWithObject.entries(map).filter(e => e.key === 'key2').map(e => console.log('    ', e))
+  }
+
+  // Nice to find a neat solution not implementing this
+  static entries (map) { return Object.keys(map).map(key => { return {key: key, value: map[key]} }) }
+}
+
+console.log('MapWithObject----------------------------------------')
+MapWithObject.runTest()
+
